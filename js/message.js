49b9a373d639247b855cb87436ba681418cc65ce
jerry.liu <jerry.liu@104.com.tw>
2019-02-26 14:33:34 +0800

    init

---
+++ js/message.js
@@ -0,0 +1,80 @@
+$(function() {
+    var customId = $("#customId").val(); // customId
+    var eventId = $("#eventId").val(); // eventId
+    var url = '/api/getMessageList?customId='+customId+'&event='+eventId;
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: function(resp) {
+            // var j = 0
+            // for (j = 0; j < 6; j++) { 
+            //     resp.push(resp[0])
+            // }
+
+            var pcMsg = $("#pcMsg");
+            var mMsg = $("#mMsg");
+            var pcFragment = $(document.createDocumentFragment());
+            var mFragment = $(document.createDocumentFragment());
+            var i = 0;
+            var pcMsgBoardDiv = null;
+            var mMsgBoardDiv = null;
+            for (i = 0; i < resp.length; i++) { 
+                var content = resp[i];
+
+                // create pc message-board
+                if(i % 10 === 0) {
+                    pcMsgBoardDiv = $("<div>").attr('class', 'message-board');
+                }
+                
+                pcMsgBoardDiv.append(createMessage(content));
+
+                // insert pc message-board to fragment
+                if(i % 10 === 0) {
+                    pcFragment.append(pcMsgBoardDiv);
+                }
+
+                // create mobile message-board
+                if(i % 3 === 0) {
+                    mMsgBoardDiv = $("<div>").attr('class', 'message-board');
+                }
+
+                mMsgBoardDiv.append(createMessage(content));
+
+                // create mobile message-board to fragment
+                if(i % 3 === 0) {
+                    mFragment.append(mMsgBoardDiv);
+                }
+            }
+
+            pcMsg.append(pcFragment);
+            mMsg.append(mFragment);
+        },
+        error: function(xhr) {
+            alert('Ajax request 發生錯誤');
+        }
+    });
+})
+
+// create msg content
+function createMessage(content) {
+    
+    var messageSpan = $("<span>").attr('class', 'message');
+        var thumbtackSpan = $("<span>").attr('class', 'thumbtack');
+        var questionSpan = $("<span>").attr('class', 'question-box');
+            var qNameWrapSpan = $("<span>").attr('class', 'name');
+                var qImgSpan = $("<i>").append($("<img>").attr('src', 'img/page/page/section8-icon/1.png'));
+                var qNameSpan = $("<span>").text(content.FullName.S);
+            qNameWrapSpan.append(qImgSpan).append(qNameSpan);
+            var qMsgWrapSpan = $("<span>").attr('class', 'question').text(content.Msg.S);
+        questionSpan.append(qNameWrapSpan).append(qMsgWrapSpan);
+        var answerSpan = $("<span>").attr('class', 'answer-box');
+            var aWrapSpan = $("<span>").attr('class', 'name');
+                var aImgSpan = $("<i>").append($("<img>").attr('src', 'img/page/page/section8-icon/hr.png'));
+                var aReply1Span = $("<span>").text('HR回覆');
+                var aReply2Span = $("<span>").attr('class', 'question').text(content.Reply.S);
+            aWrapSpan.append(aImgSpan).append(aReply1Span).append(aReply2Span);
+        answerSpan.append(aWrapSpan);
+    messageSpan.append(thumbtackSpan).append(questionSpan).append(answerSpan);
+
+    return messageSpan;
+}
---
+++ js/submit.js
@@ -0,0 +1,62 @@
+$(function() {
+    //送出留言表單
+    $("#qusetionForm").submit(function(event) {
+        cancelHandler(event)
+        //防呆判斷
+        var wkName =  $("#qusetionForm #name").val(); //姓名
+        var wkEmail =  $("#qusetionForm #email").val(); //Email
+        var wkSchool = $("#qusetionForm #school").val(); //就讀＆畢業學校
+        var wkDepartment = $("#qusetionForm #department").val(); //系所
+        var wkQstyle = $("#Qstyle").val(); //問題類型
+        var wkQdetail = $("#Qdetail").val(); //問題內容
+        var wkAgree = $("#agree").prop("checked") // true = 勾選同意
+        var isNiming = $("#isNiming").prop("checked") //true = 私密留言
+        var customId = $("#customId").val(); // customId
+        var eventId = $("#eventId").val(); // eventId
+        var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (wkName==0 || wkEmail==0 || wkQstyle==0) {
+            alert('請確認已填寫所有必填欄位')
+        }else {
+            if(!regex.test(wkEmail)) {
+                alert('e-mail格式不正確，請重新確認')
+            }else {
+                $("#qusetionForm .btn.submit").addClass('disabled')
+                //接口
+                $.ajax({
+                    url: '/api/addMessage',
+                    type: 'POST',
+                    data: {
+                        CustomId: customId,
+                        Event: eventId,
+                        FullName: wkName,
+                        Email: wkEmail,
+                        School: wkSchool,
+                        Major: wkDepartment,
+                        MsgType: wkQstyle,
+                        Msg: wkQdetail,
+                        IsShow: isNiming ? 'false' : 'true'
+                    },
+                    success: function(response) {
+                        alert('謝謝你的留言，請靜待HR的回覆。')
+                    },
+                    error: function(xhr) {
+                        alert('Ajax request 發生錯誤');
+                    },
+                    complete: function() {
+                        $("#qusetionForm .btn.submit").removeClass('disabled')
+                            window.location.reload(true);
+                    }
+                });
+            }
+        }
+    });
+
+    //手機留言板字級bug
+    setTimeout(function() {
+        $(".message-board-box.mobile .message-board .message span").css('display', 'inline-block');
+    },1000)
+    setTimeout(function() {
+        $(".message-board-box.mobile .message-board .message span").css('display', '');
+    },1100)
+
+})
